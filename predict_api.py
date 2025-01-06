a8711929385e742177a373890e21a219a4c764a6
Alex BORDARAUD <100122553+elfibro@users.noreply.github.com>
2025-01-06 20:03:48 +0100

    Update predict_api.py
    add mood

--- predict_api.py
+++ predict_api.py
@@ -18,6 +18,10 @@ class Predictor:
     def __init__(self):
         self.embedding_model_file = "./models/discogs-effnet-bs64-1.pb"
         self.classification_model_file = "./models/genre_discogs400-discogs-effnet-1.pb"
+        self.approachability_2c_model_file = "./models/approachability_2c-discogs-effnet-1.pb"
+        self.approachability_3c_model_file = "./models/approachability_3c-discogs-effnet-1.pb"
+        self.approachability_regression_model_file = "./models/approachability_regression-discogs-effnet-1.pb"
+
         self.output = "activations"
         self.sample_rate = 16000
 
@@ -32,10 +36,29 @@ class Predictor:
             input="serving_default_model_Placeholder",
             output="PartitionedCall:0",
         )
+        self.approachability_2c_model = TensorflowPredict2D(
+            graphFilename=self.approachability_2c_model_file,
+            output="model/Identity",
+        )
+        self.approachability_3c_model = TensorflowPredict2D(
+            graphFilename=self.approachability_3c_model_file,
+            output="model/Identity",
+        )
+        self.approachability_regression_model = TensorflowPredict2D(
+            graphFilename=self.approachability_regression_model_file,
+            output="model/Identity",
+        )
 
     def check_model_files_exist(self):
-        return os.path.exists(self.embedding_model_file) and os.path.exists(
-            self.classification_model_file
+        return all(
+            os.path.exists(model_file)
+            for model_file in [
+                self.embedding_model_file,
+                self.classification_model_file,
+                self.approachability_2c_model_file,
+                self.approachability_3c_model_file,
+                self.approachability_regression_model_file,
+            ]
         )
 
     def load_audio_from_file(self, file):
@@ -72,29 +95,39 @@ class Predictor:
         genre_secondary_full = sorted_genres[1][0]
         genre_secondary = genre_secondary_full.split("---")[1].strip()
 
-        return genre_primary, genre_full, genre_secondary
+        # Predicting Approachability
+        approachability_2c = self.approachability_2c_model(embeddings)
+        approachability_3c = self.approachability_3c_model(embeddings)
+        approachability_regression = self.approachability_regression_model(embeddings)
+
+        return {
+            "Primary Genre": genre_primary,
+            "Full Genre": genre_full,
+            "Secondary Genre": genre_secondary,
+            "Approachability 2C": approachability_2c.tolist(),
+            "Approachability 3C": approachability_3c.tolist(),
+            "Approachability Regression": approachability_regression.tolist(),
+        }
 
 
 predictor = Predictor()
 
 
 @app.post("/predict/")
-async def predict_genre(audio_file: UploadFile):
+async def predict_genre_and_approachability(audio_file: UploadFile):
     # Check if the uploaded file is an audio file
-    if audio_file.filename.endswith((".mp3", ".wav")):
-        # Save the uploaded file temporarily
-        audio_path = "temp_audio.wav"
-        with open(audio_path, "wb") as audio_data:
-            audio_data.write(audio_file.file.read())
+    if not audio_file.filename.endswith((".mp3", ".wav")):
+        return {"error": "Invalid file type. Please upload a .mp3 or .wav file."}
+
+    # Save the uploaded file temporarily
+    audio_path = "temp_audio.wav"
+    with open(audio_path, "wb") as audio_data:
+        audio_data.write(audio_file.file.read())
 
     try:
-        genre_primary, genre_full, genre_secondary = predictor.predict(audio_path)
+        result = predictor.predict(audio_path)
     finally:
         # Clean up temporary audio file
         os.remove(audio_path)
 
-    return {
-        "Primary Genre": genre_primary,
-        "Full Genre": genre_full,
-        "Secondary Genre": genre_secondary,
-    }
+    return result
